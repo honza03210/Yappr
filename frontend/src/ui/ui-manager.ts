7c38cb58451a889c8fcfd9a3276edca1a6ebdadf
Jan Altrichter <xaltrich@fi.muni.cz>
2026-05-13 19:20:00 +0200

    experiment with the jitter buffer size

--- frontend/src/ui/ui-manager.ts
+++ frontend/src/ui/ui-manager.ts
@@ -102,7 +102,7 @@ export class UIManager {
     }
 
     static async initAudio(){
-        this.appUI.audioCtx = new AudioContext();
+        this.appUI.audioCtx = new AudioContext({ latencyHint: "interactive", sampleRate: 48000 });
         const stream = await navigator.mediaDevices
             .getUserMedia({
                 audio: {
